37a237ebd33a6018bad7736f8e03ebdd5c56f9a8
Stew <itsstew2002@gmail.com>
2026-01-04 15:20:01 +0545

    updated the username generation

--- src/hooks/use-username.ts
+++ src/hooks/use-username.ts
@@ -1,32 +1,63 @@
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 
-const ANIMALS = [
-  "Wolf",
-  "Lion",
-  "Panther",
-  "Falcon",
-  "Hawk",
-  "Raven",
-  "Puma",
-  "Cobra",
-  "Viper",
-  "Python",
-  "Orca",
-  "Octopus",
-  "Wolverine",
-  "Hyena",
+const FIRST_NAMES = [
+  "Albus",
+  "Minerva",
+  "Severus",
+  "Remus",
+  "Sirius",
+  "Nymphadora",
+  "Luna",
+  "Draco",
+  "Hermione",
+  "Ronald",
+  "Ginevra",
+  "Cedric",
+  "Fleur",
+  "Viktor",
+  "Newt",
+  "Theseus",
+  "Bellatrix",
+  "Narcissa",
+  "Cornelius",
+  "Kingsley",
+];
+
+const LAST_NAMES = [
+  "Dumbledore",
+  "McGonagall",
+  "Snape",
+  "Lupin",
+  "Black",
+  "Tonks",
+  "Lovegood",
+  "Malfoy",
+  "Granger",
+  "Weasley",
+  "Diggory",
+  "Delacour",
+  "Krum",
+  "Scamander",
+  "Lestrange",
+  "Fudge",
+  "Shacklebolt",
+  "Longbottom",
+  "Bones",
+  "Crouch",
 ];
 
 const STORAGE_KEY = "chat_username";
 
 const generateUsername = () => {
-  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
-  return `anonymous-${word}-${nanoid(5)}`;
+  const firstName = FIRST_NAMES[Math.floor(Math.random() * FIRST_NAMES.length)];
+  const lastName = LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)];
+  return `${firstName}-${lastName}-${nanoid(4)}`;
 };
 
 export const useUsername = () => {
   const [username, setUsername] = useState("");
+
   useEffect(() => {
     const main = () => {
       const stored = localStorage.getItem(STORAGE_KEY);
@@ -34,12 +65,12 @@ export const useUsername = () => {
         setUsername(stored);
         return;
       }
-
       const generated = generateUsername();
       localStorage.setItem(STORAGE_KEY, generated);
       setUsername(generated);
     };
     main();
   }, []);
+
   return { username };
 };
